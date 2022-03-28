9041f7f2dc82ec6158817b2e636c5a944cc5a057
Matheus Rayol <matheusrbferreira@gmail.com>
2022-03-28 15:29:32 -0300

    Novos testes para treinamento

--- TreinamentoSelenium/src/test/java/TreinamentoSelenium/AppTest.java
+++ TreinamentoSelenium/src/test/java/TreinamentoSelenium/AppTest.java
@@ -2,6 +2,9 @@ package TreinamentoSelenium;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertTrue;
 
@@ -38,4 +41,47 @@ public class AppTest
         Assert.assertEquals("Validação do valor na variável fruta", "Laranja", fruta);
     }
 
+    // Teste Hello World
+    @Test
+    public void testeHelloWorld() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.google.com.br");
+
+        driver.quit();
+    }
+
+    @Test
+    public void localizarElementosLoginMantis() {
+        String url = "https://mantis-prova.base2.com.br/login_page.php";
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to(url);
+
+        driver.findElement(By.name("username"));
+        driver.findElement(By.name("password"));
+
+        driver.findElement(By.className("button"));
+
+        driver.quit();
+    }
+
+    @Test
+    public void localizarElementosLoginMantisPorXpath() {
+        String url = "https://mantis-prova.base2.com.br/login_page.php";
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to(url);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.findElement(By.xpath("//*[@name='username']"));
+        driver.findElement(By.xpath("//*[@name='password']"));
+
+        driver.findElement(By.xpath("//*[@class='button']"));
+
+        driver.quit();
+    }
 }
