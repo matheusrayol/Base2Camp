be614b564882ee33b82010d39fcdd63c424b1a32
Matheus Rayol <matheusrbferreira@gmail.com>
2022-03-28 13:57:15 -0300

    Exemplos de assertTrue e assertEquals

--- TreinamentoSelenium/src/test/java/TreinamentoSelenium/AppTest.java
+++ TreinamentoSelenium/src/test/java/TreinamentoSelenium/AppTest.java
@@ -1,20 +1,41 @@
 package TreinamentoSelenium;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    // Exemplos de assertTrue
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
     }
+
+    @Test
+    public void meuPrimeiroTeste() {
+        String fruta = "Pera";
+
+        Assert.assertTrue(fruta.equals("Pera"));
+    }
+
+    @Test
+    public void meuSegundoTeste() {
+        String carro = "Uno";
+
+        Assert.assertTrue(carro.equals("Uno"));
+    }
+
+    // Exemplos de assertEquals
+    @Test
+    public void testeComAssertEquals() {
+        String fruta = "Pera";
+
+        Assert.assertEquals("Validação do valor na variável fruta", "Laranja", fruta);
+    }
+
 }
